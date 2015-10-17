e7f3f6eb1568fbcb843abffc6daa284db0202356
freydis12 <freydis12@ru.is>
2015-10-17 22:54:18 +0000

    second test one number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,12 +3,12 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		//if(text.equals("")) {
+		if(text.equals("")) {
 			return 0;
 		}
 /*		else if(text.contains(",")){
 			return sum(splitNumbers(text));
-		}
+		}*/
 		else
 			return 1;
 	}
